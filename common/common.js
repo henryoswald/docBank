b56db753c709b6d8d623e2baae764c63fe8a1862
Henry Oswald <henry.oswald@gmail.com>
2011-06-11 15:19:56 +0100

    added procfile for heroku

--- common/common.js
+++ common/common.js
@@ -7,6 +7,6 @@ exports.dateFormater = function (date){
 	return month_names[date.getMonth()]+' '+date.getDate()+' '+date.getFullYear()
 }
 
-module.exports.email = require('./utils/email');
-module.exports.settings = require('./config/settings');
+module.exports.email = require('./utils/Email');
+module.exports.settings = require('./config/Settings');
 module.exports.mongoose =  require('./config/Mongo_Connection');
